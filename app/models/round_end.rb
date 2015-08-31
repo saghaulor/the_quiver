8867ee6b3b15720b77bcaa41b5f57f728fed93d4
Stephen Aghaulor <saghaulor@gmail.com>
2015-08-30 21:43:22 -0700

    Added RoundEnd model.
    - End size should probably be dictated as an attribute on the round_end,
      and not as a class method. This will make it more compatible with
      custom ends that may have sizes supplied via user input.

--- app/models/round_end.rb
+++ app/models/round_end.rb
@@ -1,12 +1,33 @@
 class RoundEnd < ActiveRecord::Base
   belongs_to :round
-  belongs_to :uom
 
   before_validation :set_end_score, :set_end_x_count
-  validates :uom_id, presence: true
 
-  def shots_list
-    shots.to_s
+  def self.end_size
+    6
+  end
+
+  enum uom: {
+    yd: 'yd',
+    m: 'm',
+    ft: 'ft'
+  }
+
+  validates :shots, length: { maximum: end_size } # End size must be defined before the validation can be called.
+  validates :distance, :uom, presence: true
+
+  def size
+    self.class.end_size
+  end
+
+  def end_average
+    (score.to_f / self.class.end_size).round(2)
+  end
+
+  def calculate_x_count
+    shots.inject(0) do |sum, shot|
+      sum += shot.downcase.eql?("x") ? 1 : 0
+    end
   end
 
   def calculate_end_score(x_val = 10)
@@ -16,22 +37,13 @@ class RoundEnd < ActiveRecord::Base
     end
   end
 
-  def end_x_count
-    shots.inject(0) do |sum, shot|
-      sum += shot.downcase.eql?("x") ? 1 : 0
-    end
-  end
+  private
 
   def set_end_score
     self.score= calculate_end_score
   end
 
   def set_end_x_count
-    self.x_count= end_x_count
-  end
-
-  def uom
-    uom = super
-    uom.unit
+    self.x_count= calculate_x_count
   end
 end
--- test/models/round_end_test.rb
+++ test/models/round_end_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
 class RoundEndTest < ActiveSupport::TestCase
-  def setup
-    initialize_end
-  end
-
   test "calculate_end_score" do
     assert_same @end.calculate_end_score, 10
   end
 
-  test "end_x_count" do
-    assert_same @end.end_x_count, 1
+  test "calculate_end_score_with_x_eql_5" do
+    assert_same @end.calculate_end_score(5), 5
+  end
+
+  test "calculate_x_count" do
+    assert_same @end.calculate_x_count, 1
   end
 
   test "set_end_score" do
@@ -21,9 +21,22 @@ class RoundEndTest < ActiveSupport::TestCase
     assert_same @end.x_count, 1
   end
 
+  test "end_average" do
+    assert_in_delta @end.end_average, (10.to_f / @end.shots.size), 0.01
+  end
+
+  test "size" do
+    assert_same @end.size, @end.class.end_size
+  end
+
   private
-    def initialize_end
-      @end = RoundEnd.new(shots: [0,0,0,0,0,'x'])
-      @end.valid?
-    end
+  def setup
+    DatabaseCleaner.start
+    @end = RoundEnd.new(shots: [0,0,0,0,0,'x'])
+    @end.valid?
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
 end
