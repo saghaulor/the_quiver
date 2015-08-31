6811e754e3b21ab2f9c0cec8631b75ca8f82c307
Stephen Aghaulor <saghaulor@gmail.com>
2015-08-30 21:37:03 -0700

    Add test helper methods, simplecov,  and db cleaner.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,19 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'tmp/'
+  add_filter 'log/'
+  add_filter 'bin/'
+  add_filter 'coverage/'
+end
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :transaction
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
@@ -11,13 +22,23 @@ require "minitest/rails"
 # require "minitest/pride"
 
 class ActiveSupport::TestCase
-    ActiveRecord::Migration.check_pending!
+  ActiveRecord::Migration.check_pending!
 
-    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
 
+  def json(json)
+    JSON.parse(json, symbolize_names: true)
+  end
+
+  def request_headers
+    {
+      "Accept" => "application/json",
+      "Content-Type" => "application/json"
+    }
+  end
   # Add more helper methods to be used by all tests here...
 end
