6a24c8701dd2b9cb74a0ebe1b02fcbdf8bfd4800
Stephen Aghaulor <saghaulor@gmail.com>
2015-08-30 21:49:02 -0700

    Require only that which is required for a JSON API.
    - Configure db dumps to dump pg structure instead of ruby because AR
      doesn't support dumping pg enums.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,6 +26,7 @@ module TheQuiver
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.schema_format = :sql
     config.generators do |generate|
       generate.test_framework :minitest, spec: false, fixture: false
       generate.helper false
