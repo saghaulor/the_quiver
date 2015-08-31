ee205860958a51c1eef797676a5e0b3b855ffdcc
Stephen Aghaulor <saghaulor@gmail.com>
2015-08-30 21:40:47 -0700

    Added Round model and tests.

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,8 +1,47 @@
 class Round < ActiveRecord::Base
-  has_many :round_ends
+  has_many :round_ends, dependent: :destroy
+  # ^^^^^ This should be specified in the ancestor models.
   belongs_to :user
 
+  validates :name, presence: true
+  validates :age_division, presence: true
+
+   enum age_division: {
+          cub: 'cub',
+          youth: 'youth',
+          young_adult: 'young_adult',
+          adult: 'adult',
+          senior: 'senior',
+          silver_senior: 'silver_senior',
+          master_senior: 'master_senior'
+   }
+
   def score
-    self.round_ends.inject(0) { |sum, re| sum += re.score }
+    round_ends.inject(0) { |sum, re| sum += re.score }
+  end
+
+  def x_count
+    round_ends.inject(0) { |sum, re| sum += re.x_count }
+  end
+
+  def average
+    if round_ends.empty?
+      nil.to_f
+    else
+      sum = round_ends.inject(0) { |sum, re| sum += re.end_average }
+      sum.to_f / end_count
+    end
+  end
+
+  def end_count
+    round_ends.count
+  end
+
+  def can_mulligan?
+    false
+  end
+
+  def create_ends
+    nil
   end
 end
--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -1,18 +1,47 @@
 require 'test_helper'
 
 class RoundTest < ActiveSupport::TestCase
-  def setup
-    initialize_round_and_ends
-  end
-  
   test "score" do
     assert_same @round.score, 20
   end
 
+  test "x_count" do
+    assert_same @round.x_count, 2
+  end
+
+  test "average" do
+    shots = @round.round_ends.inject(0) {|sum, round| sum += round.size}
+    assert_in_delta @round.average, (20.to_f / shots), 0.01
+  end
+
+  test "average_without_ends" do
+    assert_same Round.new.average, 0.0
+  end
+
+  test "create_ends" do
+    assert_nil Round.new.create_ends
+  end
+
+  test "determine_age_division" do
+    skip
+    # user = user.new
+    # user.stub(dob: date.today - 19.years) do
+    #   round = round.new(user: user)
+    #   assert_equal round.age_division, 'adult'
+    # end
+
+  end
+
   private
-    def initialize_round_and_ends
-      @round = Round.create(name: 'Test Round')
-      RoundEnd.create(round: @round, shots: [0,0,0,0,0,'x'])
-      RoundEnd.create(round: @round, shots: [0,0,0,0,0,'x'])
-    end
+  def setup
+    DatabaseCleaner.start
+    @round = Round.create(name: 'Test Round')
+    attrs = { round: @round, shots: [0,0,0,0,0,'x'], distance: 50, uom: 'yd' }
+    RoundEnd.create(attrs)
+    RoundEnd.create(attrs)
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
 end
